b13b80081d54e69603fd40b1cd13474da605b0fc
thomaswright22 <32115521+thomaswright22@users.noreply.github.com>
2017-09-25 11:24:50 -0400

    Create doing-more.js

---
+++ doing-more.js
@@ -0,0 +1,6 @@
+function moreStuff(){
+document.getElementById("solution").innerHTML="<p>A paragraph</p><h2>heading</h2>"+"<p>another paragraph"+ "with a <a href='http://www.cvuhs.org'> link</a></p>"
+}
+function jazzing(){
+document.getElementById("responce").innerHTML="<h1>YOU'VE BEEN FOOLED!!!!</h1>"
+}
